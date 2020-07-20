43552e2ee5cd263c01cdfdba380509901bbeb3c5
Bart Stéphane <stephane.bart@he-arc.ch>
2020-07-20 15:59:58 +0200

    update main.js add bootswatch theme

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ window.$ = $;
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootswatch/dist/simplex/bootstrap.min.css";
 import "./main.css";
 $(document).ready(() => {
   console.log("it works!");
